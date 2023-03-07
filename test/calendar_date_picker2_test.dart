c281469db0b7f0be57ce6f5ce779eb13dc6536f0
Samuel Ong <samuelong135@gmail.com>
2023-03-07 16:43:39 +0800

    fix: #75

--- test/calendar_date_picker2_test.dart
+++ test/calendar_date_picker2_test.dart
@@ -1,5 +1,16 @@
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Calendar date picker2 unit test', () {});
+  testWidgets('Empty initial list wont throw unmodified list',
+      (widgetTester) async {
+    await widgetTester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: CalendarDatePicker2(
+            initialValue: const [], config: CalendarDatePicker2Config()),
+      ),
+    ));
+  });
 }
