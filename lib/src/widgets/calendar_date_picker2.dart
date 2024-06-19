8d2244866061a9d9bf1a806c3cd32b851b8d553a
Neo Liu <n.liu@bettechnology.com>
2024-06-19 21:18:02 +1000

    release:v1.1.2

--- lib/src/widgets/calendar_date_picker2.dart
+++ lib/src/widgets/calendar_date_picker2.dart
@@ -22,7 +22,6 @@ const Duration _monthScrollDuration = Duration(milliseconds: 200);
 
 const double _dayPickerRowHeight = 42.0;
 const int _maxDayPickerRowCount = 6; // A 31 day month that starts on Saturday.
-// One extra row for the day-of-week header.
 const double _monthPickerHorizontalPadding = 8.0;
 
 const int _yearPickerColumnCount = 3;
